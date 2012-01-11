e1d465e366e62c1866f4d8e440b15a6e39f2b09c
Alex Gallardo <elmankisimo@gmail.com>
2012-01-11 17:50:47 -0600

    updating

--- functions.js
+++ functions.js
@@ -13,48 +13,48 @@ var img2= new Image();
 var collision = 0;
 
 function rect(x,y,w,h) {
-ctx.beginPath();
-ctx.rect(x,y,w,h);
-ctx.closePath();
-ctx.fill();
-//exactly the same thing for layer2
-ctxl2.beginPath();
-ctxl2.rect(x,y,w,h);
-ctxl2.closePath();
-ctxl2.fill();
+	ctx.beginPath();
+	ctx.rect(x,y,w,h);
+	ctx.closePath();
+	ctx.fill();
+	//exactly the same thing for layer2
+	ctxl2.beginPath();
+	ctxl2.rect(x,y,w,h);
+	ctxl2.closePath();
+	ctxl2.fill();
 }
 
 function clear() {
-ctx.clearRect(0, 0, WIDTH, HEIGHT);
-ctx.drawImage(img, 0, 0);
-ctxl2.clearRect(0,0,WIDTH,HEIGHT);
-ctxl2.drawImage(img2,0,0);//load de full color graphics
+	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+	ctx.drawImage(img, 0, 0);
+	ctxl2.clearRect(0,0,WIDTH,HEIGHT);
+	ctxl2.drawImage(img2,0,0);//load de full color graphics
 }
 
 function init() {
-canvas = document.getElementById("canvas");
-layer2=document.getElementById("layer2");
-ctx = canvas.getContext("2d");
-ctxl2=layer2.getContext("2d");
-img.src = "images/path.png";
-img2.src = "images/SizeRef.jpg";
-return setInterval(draw, 10);
+	canvas = document.getElementById("canvas");
+	layer2=document.getElementById("layer2");
+	ctx = canvas.getContext("2d");
+	ctxl2=layer2.getContext("2d");
+	img.src = "images/path.png";
+	img2.src = "images/SizeRef.jpg";
+	return setInterval(draw, 10);
 }
 
 function doKeyDown(evt){
-switch (evt.keyCode) {
-case 38:  /* Up arrow was pressed */
-if (y - dy > 0){
-y -= dy;
-clear();
-checkcollision();
-if (collision == 1){
-y += dy;
-collision = 0;
-}
-}
+	switch (evt.keyCode) {
+		case 38:  /* Up arrow was pressed */
+		if (y - dy > 0){
+			y -= dy;
+			clear();
+			checkcollision();
+		if (collision == 1){
+			y += dy;
+			collision = 0;
+		}
+	}	
 
-break;
+	break;
 case 40:  /* Down arrow was pressed */
 if (y + dy < HEIGHT ){
 y += dy;
